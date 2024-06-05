3c90e219c06a0ce7f94fe400e8ef549bafaa4fd9
willl85 <ngowilfried@gmail.com>
2024-06-05 23:01:20 +0200

    Create head component and update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from 'next/font/google'
+import Header from '@/components/layouts/header/Header'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from './head'
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head />
+      <body>
+          <Header />
+          <main>{children}</main>
+      </body>
     </html>
   )
 }
