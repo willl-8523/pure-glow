d48b245abcf09dedd734c7e4dca40cf07c291272
willl85 <ngowilfried@gmail.com>
2024-06-06 16:27:46 +0200

    Add navigations and style them

--- components/UI/nav-links/NavLinks.jsx
+++ components/UI/nav-links/NavLinks.jsx
@@ -1,36 +1,52 @@
 'use client';
 import Link from 'next/link';
+import { FaSearch, FaShoppingCart } from 'react-icons/fa';
 import classes from './NavLinks.module.css';
+import { usePathname } from 'next/navigation';
 
 export default function NavLinks() {
-  const links = [
-    {
-      name: 'Accueil',
-      href: '/accueil',
-    },
-    {
-      name: 'Boutique',
-      href: '/boutique',
-    },
-    {
-      name: 'A Propos',
-      href: '/a-propos',
-    },
-    {
-      name: 'Blog',
-      href: '/blog',
-    },
-  ];
-
+  const pathname = usePathname();
+  
   return (
-    <>
-      {links.map((link) => {
-        return (
-          <Link key={link.name} href={link.href} className={classes.nav__links}>
-            <p className="hidden md:block">{link.name.toUpperCase()}</p>
-          </Link>
-        );
-      })}
-    </>
+    <ul className={classes.nav__links}>
+      <li
+        className={`${classes.nav__link} ${
+          pathname === '/accueil' ? classes.active : ''
+        }`}
+      >
+        <Link href="/accueil">ACCUEIL</Link>
+      </li>
+      <li
+        className={`${classes.nav__link} ${
+          pathname === '/boutique' ? classes.active : ''
+        }`}
+      >
+        <Link href="/boutique">BOUTIQUE</Link>
+      </li>
+      <li
+        className={`${classes.nav__link} ${
+          pathname === '/a-propos' ? classes.active : ''
+        }`}
+      >
+        <Link href="/a-propos">A PROPOS</Link>
+      </li>
+      <li
+        className={`${classes.nav__link} ${
+          pathname === '/blog' ? classes.active : ''
+        }`}
+      >
+        <Link href="/blog">BLOG</Link>
+      </li>
+      <li>
+        <Link href="">
+          <FaSearch />
+        </Link>
+      </li>
+      <li>
+        <Link href="/panier">
+          <FaShoppingCart />
+        </Link>
+      </li>
+    </ul>
   );
 }
