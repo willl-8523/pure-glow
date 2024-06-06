bbbcf6d07ff70f345945c2867ea391b28690d489
willl85 <ngowilfried@gmail.com>
2024-06-06 16:25:02 +0200

    Replace image element  to text 'Pure glow'

--- components/UI/logo/Logo.jsx
+++ components/UI/logo/Logo.jsx
@@ -1,17 +1,7 @@
 'use client';
 
-import Image from 'next/image';
-import React from 'react'
-import classes from './Logo.module.css'
+import classes from './Logo.module.css';
 
 export default function Logo() {
-  return (
-    <Image
-      src="/images/logo.png"
-      width={300}
-      height={200}
-      alt="Logo of the pure_glow application"
-      className={classes.logo}
-    />
-  );
+  return <div className={classes.logo}>Pure glow</div>;
 }
