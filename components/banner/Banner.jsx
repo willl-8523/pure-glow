56f23b6725095ad016621b0fe6ef3c801cd58256
willl85 <ngowilfried@gmail.com>
2024-06-06 16:29:23 +0200

    Add banner content and banner navigation and style them

--- components/banner/Banner.jsx
+++ components/banner/Banner.jsx
@@ -5,7 +5,17 @@ import classes from './Banner.module.css';
 export default function Banner() {
   return (
     <div className={classes.banner}>
-      <img src="/images/banner__img.svg" alt="pure glow banner_image" />
+      <div className={classes.banner__content}>
+        <div className={classes.banner__slider}>
+          <h1>Pure Clow : Révélez la Beauté Naturelle de Votre Peau</h1>
+          <p>Une Harmonie Parfaite entre Pureté et Éclat</p>
+        </div>
+      </div>
+      <div className={classes.banner__navigation}>
+        <span className={classes.active}></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   );
 }
